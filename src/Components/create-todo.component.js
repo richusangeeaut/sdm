aa8672ec47ad6e5f61d25e9b48bddb057fdf1c88
richusangeeaut <53170250+richusangeeaut@users.noreply.github.com>
2019-08-16 14:58:39 +1200

    Add files via upload

--- src/Components/create-todo.component.js
+++ src/Components/create-todo.component.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 export default class CreateTodo extends Component {
+
     constructor(props) {
         super(props);
+
         this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
         this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
         this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+
         this.state = {
             todo_description: '',
             todo_responsible: '',
@@ -13,21 +17,25 @@ export default class CreateTodo extends Component {
             todo_completed: false
         }
     }
+
     onChangeTodoDescription(e) {
         this.setState({
             todo_description: e.target.value
         });
     }
+
     onChangeTodoResponsible(e) {
         this.setState({
             todo_responsible: e.target.value
         });
     }
+
     onChangeTodoPriority(e) {
         this.setState({
             todo_priority: e.target.value
         });
     }
+
     onSubmit(e) {
         e.preventDefault();
         
@@ -35,7 +43,17 @@ export default class CreateTodo extends Component {
         console.log(`Todo Description: ${this.state.todo_description}`);
         console.log(`Todo Responsible: ${this.state.todo_responsible}`);
         console.log(`Todo Priority: ${this.state.todo_priority}`);
-        
+     
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        };
+
+        axios.post('http://localhost:4000/todos/add', newTodo)
+            .then(res => console.log(res.data));
+
         this.setState({
             todo_description: '',
             todo_responsible: '',
@@ -43,6 +61,7 @@ export default class CreateTodo extends Component {
             todo_completed: false
         })
     }
+
     render() {
         return (
             <div style={{marginTop: 10}}>
@@ -100,6 +119,7 @@ export default class CreateTodo extends Component {
                             <label className="form-check-label">High</label>
                         </div>
                     </div>
+
                     <div className="form-group">
                         <input type="submit" value="Create Todo" className="btn btn-primary" />
                     </div>
--- src/Components/todos-list.component.js
+++ src/Components/todos-list.component.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Todo = props => (
     <tr>
-        <td className={props.todo.todo_completed ? 'completed' : ''}>
-            {props.todo.todo_description}
-        </td>
-        <td className={props.todo.todo_completed ? 'completed' : ''}>
-            {props.todo.todo_responsible}
-        </td>
-        <td className={props.todo.todo_completed ? 'completed' : ''}>
-        {props.todo.todo_priority}
-        </td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
         <td>
-            <Link to={"/edit/" + props.todo._id} >Edit </Link>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
         </td>
     </tr>
 )
 
 export default class TodosList extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {todos: []};
     }
@@ -29,47 +23,37 @@ export default class TodosList extends Component {
     componentDidMount() {
         axios.get('http://localhost:4000/todos/')
             .then(response => {
-                this.setState({todos: response.data});
+                this.setState({ todos: response.data });
             })
-            .catch(function (error) {
+            .catch(function (error){
                 console.log(error);
             })
     }
 
-        
-    todoList(){
-        return this.state.todos.map(function(currenttodo, i){
-            return <Todo todo= {currenttodo} key = {i}/>
-        });
+    todoList() {
+        return this.state.todos.map(function(currentTodo, i){
+            return <Todo todo={currentTodo} key={i} />;
+        })
     }
 
     render() {
-        return(
+        return (
             <div>
                 <h3>Todos List</h3>
-                <table className="table table-striped" style={{marginTop:20}} >
+                <table className="table table-striped" style={{ marginTop: 20 }} >
                     <thead>
                         <tr>
-                            <th>
-                                Description
-                            </th>
-                            <th>
-                                Responsible
-                            </th>
-                            <th>
-                                Priority
-                            </th>
-                            <th>
-                                Action
-                            </th>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {  this.todoList() }
+                        { this.todoList() }
                     </tbody>
                 </table>
             </div>
         )
     }
 }
-   
